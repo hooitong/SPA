691e2e149331a5f7449fbd6df19de03704acacab
Jason Sia <swkjason@hotmail.com>
2015-03-20 21:46:22 +0800

    Update ProcTable header files

--- EmptyGeneralTesting/source/ProcTable.h
+++ EmptyGeneralTesting/source/ProcTable.h
@@ -1,8 +1,33 @@
 #pragma once
+
+
+#include <vector>
+
+using namespace std;
+
+typedef int PROCINDEX, VARINDEX;
+typedef string PROCNAME;
+
+#include <TNode.h>
+
+
 class ProcTable
 {
 public:
 	ProcTable(void);
 	~ProcTable(void);
+
+	PROCINDEX insertProc(PROCNAME procName);
+	PROCINDEX getProcINdex(PROCNAME procName);
+	PROCNAME getProcName(PROCINDEX procIndex);
+	vector<PROCNAME> getAllProc();
+	bool setTRoot(PROCINDEX index, TNode node);
+	bool setGRoot(PROCINDEX index, GNode node);
+	TNode getTRoot(PROCINDEX index);
+	GNode getGNode(PROCINDEX index);
+	bool addModified(PROCINDEX proc, VARINDEX var);
+	bool addUsed(PROCINDEX proc, VARINDEX var);
+	vector<VARINDEX> getModified(PROCINDEX proc);
+	vector<VARINDEX> getUsed(PROCINDEX proc);
 };
 
