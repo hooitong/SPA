20bff93d9aaed2a531033439d1c080ff9a259fa0
Jason Sia <swkjason@hotmail.com>
2015-03-21 17:20:09 +0800

    Update Uses Header and cpp

--- EmptyGeneralTesting/source/Uses.cpp
+++ EmptyGeneralTesting/source/Uses.cpp
@@ -5,6 +5,17 @@ Uses::Uses(void)
 {
 }
 
+bool setUsesInProc(PROCINDEX procIndex, VARINDEX varIndex){
+}
+bool setUsesInStmt(STMTLine stmtLine, VARINDEX varIndex){
+}
+vector<VARINDEX> getUsedByProc (PROCINDEX procIndex){
+}
+vector<VARINDEX> getUsedByStmt (STMTLine stmtLine){
+}
+vector<TNode> getUses(VARINDEX varIndex){
+}
+
 
 Uses::~Uses(void)
 {
--- EmptyGeneralTesting/source/Uses.h
+++ EmptyGeneralTesting/source/Uses.h
@@ -1,8 +1,19 @@
 #pragma once
+
+#include <vector>
+#include "TNode.h"
+
+typedef int PROCINDEX, VARINDEX, STMTLine;
 class Uses
 {
 public:
 	Uses(void);
 	~Uses(void);
+
+	bool setUsesInProc(PROCINDEX procIndex, VARINDEX varIndex);
+	bool setUsesInStmt(STMTLine stmtLine, VARINDEX varIndex);
+	vector<VARINDEX> getUsedByProc (PROCINDEX procIndex);
+	vector<VARINDEX> getUsedByStmt (STMTLine stmtLine);
+	vector<TNode> getUses(VARINDEX varIndex);
 };
 
