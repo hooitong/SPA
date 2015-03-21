67e6208725cfffa356c2ee8085361a84fcf92a71
Jason Sia <swkjason@hotmail.com>
2015-03-21 17:12:11 +0800

    Updated Modifies header and cpp

--- EmptyGeneralTesting/source/Modifies.cpp
+++ EmptyGeneralTesting/source/Modifies.cpp
@@ -6,6 +6,17 @@ Modifies::Modifies(void)
 }
 
 
+bool setModifiesInProc(PROCINDEX procIndex, VARINDEX varIndex){
+}
+bool setModifiesInStmt(STMTLine stmt, VARINDEX varIndex){
+}
+vector<VARINDEX> getModifiedByProc(PROCINDEX procIndex){
+}
+vector<VARINDEX> getModifiedByStmt(STMTLine stmtLine){
+}
+vector<TNode> getModifies(VARINDEX varIndex){
+}
+
 Modifies::~Modifies(void)
 {
 }
--- EmptyGeneralTesting/source/Modifies.h
+++ EmptyGeneralTesting/source/Modifies.h
@@ -1,8 +1,23 @@
 #pragma once
+
+#include <vector>
+#include"TNode.h"
+
+typedef int PROCINDEX, VARINDEX, STMTLine;
+
 class Modifies
 {
 public:
 	Modifies(void);
 	~Modifies(void);
+
+	bool setModifiesInProc(PROCINDEX procIndex, VARINDEX varIndex);
+	bool setModifiesInStmt(STMTLine stmt, VARINDEX varIndex);
+	vector<VARINDEX> getModifiedByProc(PROCINDEX procIndex);
+	vector<VARINDEX> getModifiedByStmt(STMTLine stmtLine);
+	vector<TNode> getModifies(VARINDEX varIndex);
+
+
+
 };
 
