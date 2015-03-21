78d3d2ec0a6035c973d32b15c430d4b85ef2a24e
Yeap Hooi Tong <hooitongx@gmail.com>
2015-03-21 22:55:57 +0800

    First VarTable Implementation

--- EmptyGeneralTesting/source/VarTable.cpp
+++ EmptyGeneralTesting/source/VarTable.cpp
@@ -1,56 +1,103 @@
 #include "VarTable.h"
-
-//constructor
+/* Constructor & Destructor */
 VarTable::VarTable(void)
 {
-
+	currentMapIndex = 0;
 }
 
-
-
-VARINDEX insertVar(VARNAME VARNAME)
-{
-	return -1;
-}
-
-VARNAME getVARNAME(VARINDEX VARINDEX){
-	return "";
-}
-
-VARINDEX getVARINDEX(VARNAME VARNAME){
-	return -1;
-}
-
-int getSize(){
-	return -1;
-}
-
-vector<VARINDEX> getAllVar(){
-	vector<VARINDEX> VARINDEXList;
-	return VARINDEXList;
-}
-
-bool addModifiedBy(VARINDEX var){
-	return false;
-}
-
-//Need to create TNode Class First
-bool addUsedBy(VARINDEX var, TNode node){
-	return false;
-}
-
-vector<TNode> getModifiedBy(VARINDEX var){
-	vector<TNode> tNodeList;
-	return tNodeList;
-}
-
-vector<TNode> getUsedBy (VARINDEX var){
-	vector<TNode> tNodeList;
-	return tNodeList;
-}
-
-//destructor
 VarTable::~VarTable(void)
 {
 
+}
+
+VARINDEX VarTable::insertVar(VARNAME varName)
+{
+	VARINDEX v = getVarIndex(varName);
+	/* if varName already exist in varTable, return index */
+	if(v != -1){
+		return v;
+	}
+	
+	/* Create new varInfo and insert into both index and name maps */
+	varInfo variable = {};
+	variable.var_name = varName;
+	variable.var_index = currentMapIndex;
+	varIndexMap[currentMapIndex] = variable;
+	varNameMap[varName] = variable;
+
+	/* Increment the static index */
+	VARINDEX newIndex = currentMapIndex++;
+	return newIndex;
+}
+
+VARNAME VarTable::getVarName(VARINDEX varIndex){
+	/* Check if varIndex is valid */
+	if(varIndex < 0 || varIndex >= currentMapIndex) {
+		return NULL;
+	} else {
+		return varIndexMap[varIndex].var_name;
+	}
+}
+
+VARINDEX VarTable::getVarIndex(VARNAME varName){
+	map<VARNAME, varInfo>::iterator itr = varNameMap.find(varName);
+	if(itr != varNameMap.end()) {
+		return itr->second.var_index;
+	} else {
+		return -1;
+	}
+}
+
+int VarTable::getSize(){
+	return currentMapIndex;
+}
+
+vector<VARINDEX> VarTable::getAllVarIndex(){
+	vector<VARINDEX> listOfVarIndex;
+	for(int i = 0; i < currentMapIndex; i++) {
+		listOfVarIndex.push_back(i);
+	}
+	return listOfVarIndex;
+}
+
+vector<VARNAME> VarTable::getAllVarName(){
+	vector<VARNAME> listOfVarName;
+	for(int i = 0; i < currentMapIndex; i++) {
+		listOfVarName.push_back(varIndexMap[i].var_name);
+	}
+	return listOfVarName;
+}
+
+bool VarTable::addModifiedBy(VARINDEX varIndex, TNode node){
+	if(varIndex < 0 || varIndex >= currentMapIndex) {
+		return false;
+	} else {
+		varIndexMap[varIndex].modified_by.push_back(node);
+		return true;
+	}
+}
+
+bool VarTable::addUsedBy(VARINDEX varIndex, TNode node){
+	if(varIndex < 0 || varIndex >= currentMapIndex) {
+		return false;
+	} else {
+		varIndexMap[varIndex].used_by.push_back(node);
+		return false;
+	}
+}
+
+vector<TNode> VarTable::getModifiedBy(VARINDEX varIndex){
+	if(varIndex < 0 || varIndex >= currentMapIndex) {
+		//return NULL;
+	} else {
+		return varIndexMap[varIndex].modified_by; 
+	}
+}
+
+vector<TNode> VarTable::getUsedBy (VARINDEX varIndex){
+	if(varIndex < 0 || varIndex >= currentMapIndex) {
+		//return NULL;
+	} else {
+		return varIndexMap[varIndex].used_by; 
+	}
 }
--- EmptyGeneralTesting/source/VarTable.h
+++ EmptyGeneralTesting/source/VarTable.h
@@ -1,31 +1,40 @@
 #pragma once
 #include <vector>
+#include <map>
 #include "GlobalTypedef.h"
 #include "TNode.h"
 
 using namespace std;
 
-
-
+struct varInfo{
+	VARINDEX var_index;
+	VARNAME var_name;
+	std::vector<TNode> modified_by;
+	std::vector<TNode> used_by;	
+};
 
 class VarTable
 {
-
 private:
+	static int test;
+	static map<VARINDEX, varInfo> varIndexMap;
+	static map<VARNAME, varInfo> varNameMap;
+	static VARINDEX currentMapIndex;
 
 public:
 	VarTable(void);
 	~VarTable(void);
-
-	static VARINDEX insertVar(VARNAME VARNAME);
-	static VARNAME getVARNAME(VARINDEX VARINDEX);
-	static VARINDEX getVARINDEX(VARNAME VARNAME);
+	
+	static VARINDEX insertVar(VARNAME varName);
+	static VARNAME getVarName(VARINDEX varIndex);
+	static VARINDEX getVarIndex(VARNAME varName);
 	static int getSize();
-	static vector<VARINDEX> getAllVar();
-	static bool addModifiedBy(VARINDEX var, TNode node);
-	static bool addUsedBy (VARINDEX var, TNode node);
-	static vector<TNode> getModifiedBy(VARINDEX var);
-	static vector<TNode> getUsedBy(VARINDEX var);
+	static vector<VARINDEX> getAllVarIndex();
+	static vector<VARNAME> getAllVarName();
+	static bool addModifiedBy(VARINDEX varIndex, TNode node);
+	static bool addUsedBy (VARINDEX varIndex, TNode node);
+	static vector<TNode> getModifiedBy(VARINDEX varIndex);
+	static vector<TNode> getUsedBy(VARINDEX varIndex);
 
 };
 
