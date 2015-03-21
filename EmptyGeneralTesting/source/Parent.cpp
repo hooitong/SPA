1f8a2d1b1eb1d4419a01d382f890c3c705425183
Jason Sia <swkjason@hotmail.com>
2015-03-21 16:52:01 +0800

    Updated Parent header and cpp files.

--- EmptyGeneralTesting/source/Parent.cpp
+++ EmptyGeneralTesting/source/Parent.cpp
@@ -6,6 +6,21 @@ Parent::Parent(void)
 }
 
 
+bool setParent(STMTLine parent, STMTLine child){
+}
+bool isParent(STMTLine first, STMTLine second){
+}
+bool isParentStar(STMTLine first, STMTLine second){
+}
+TNode getParent(STMTLine child){
+}
+vector<TNode> getParentStar(STMTLine child){
+}
+vector<TNode> getChildOf(STMTLine parent){
+}
+vector<TNode> getChildOfStar(STMTLine parent){
+}
+
 Parent::~Parent(void)
 {
 }
--- EmptyGeneralTesting/source/Parent.h
+++ EmptyGeneralTesting/source/Parent.h
@@ -1,8 +1,23 @@
 #pragma once
+
+typedef int STMTLine;
+#include <vector>
+#include "TNode.h"
+
 class Parent
 {
 public:
 	Parent(void);
 	~Parent(void);
+
+	bool setParent(STMTLine parent, STMTLine child);
+	bool isParent(STMTLine first, STMTLine second);
+	bool isParentStar(STMTLine first, STMTLine second);
+	TNode getParent(STMTLine child);
+	vector<TNode> getParentStar(STMTLine child);
+	vector<TNode> getChildOf(STMTLine parent);
+	vector<TNode> getChildOfStar(STMTLine parent);
+
+
 };
 
