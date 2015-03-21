180eb61afc9c8ed5d6fe7f6b1aee473c6b3a62ce
Jason Sia <swkjason@hotmail.com>
2015-03-21 16:34:41 +0800

    Updated Follows header and cpp

--- EmptyGeneralTesting/source/Follows.cpp
+++ EmptyGeneralTesting/source/Follows.cpp
@@ -5,6 +5,18 @@ Follows::Follows(void)
 {
 }
 
+TNode setFollows(STMTLine stmtLineFirst, STMTLine stmtLineSecond){
+}
+bool isFollow(STMTLine stmtLineFirst, STMTLine second){
+}
+bool isFollowStar(STMTLine stmtLineFirst, STMTLine second){
+}
+vector<TNode> getFollowedByStar(STMTLine line){
+}
+vector<TNode> getFollowsFromStar(STMTLine line){
+}
+
+
 
 Follows::~Follows(void)
 {
--- EmptyGeneralTesting/source/Follows.h
+++ EmptyGeneralTesting/source/Follows.h
@@ -1,8 +1,19 @@
 #pragma once
+
+#include "TNode.h"
+#include <vector>
+typedef int STMTLine;
+
 class Follows
 {
 public:
 	Follows(void);
 	~Follows(void);
+
+	TNode setFollows(STMTLine stmtLineFirst, STMTLine stmtLineSecond);
+	bool isFollow(STMTLine stmtLineFirst, STMTLine second);
+	bool isFollowStar(STMTLine stmtLineFirst, STMTLine second);
+	vector<TNode> getFollowedByStar(STMTLine line);
+	vector<TNode> getFollowsFromStar(STMTLine line);
 };
 
