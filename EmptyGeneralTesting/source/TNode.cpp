8e7104ef2f1ae82566017c33697ee1b538a8038c
Jason Sia <swkjason@hotmail.com>
2015-03-22 19:56:37 +0800

    Updated TNode

--- EmptyGeneralTesting/source/TNode.cpp
+++ EmptyGeneralTesting/source/TNode.cpp
@@ -1,26 +1,43 @@
 #include "TNode.h"
 
-TNode::TNode(void)
-{
 
+TNode::TNode(void){
+}
+TNode::TNode(TNodeType typeOfNode, string nodeName){
+	type = typeOfNode;
+	name = nodeName;
 }
 
-TNode::~TNode(void)
-{
-
+bool TNode::setParent(TNode node){
+	if(&parentNode){
+		return false;
+		//already has parent, prevent it from resetting parent
+	}
+	parentNode = &node;
+	return true;
 }
 
-TNode TNode::createTNode(TNodeType nodeType, string name)
-{
-	return TNode();
+bool TNode:: addSibling(TNode siblingNode){
+	
+	TNode::getSiblingsNodes().push_back(&siblingNode);
+	return true;	
 }
 
-TNodeType TNode::getTType(TNode node) 
-{
-	return TNodeType::NONE;
+vector<TNode*> TNode::getSiblingsNodes(){
+	return siblingNodes;
 }
 
-bool TNode::isTTYpe(TNode node, TNodeType type)
-{
-	return false;
+TNode::~TNode(void){
+	type = NONE;
+	parentNode = NULL;
+	childNodes.clear();
+	siblingNodes.clear();
+}
+
+TNodeType TNode::getTType() {
+	return type;
+}
+
+bool TNode::isTTYpe(TNode node, TNodeType type){
+	return (node.getTType()==type);
 }
--- EmptyGeneralTesting/source/TNode.h
+++ EmptyGeneralTesting/source/TNode.h
@@ -1,5 +1,6 @@
 #pragma once
 #include <string>
+#include <vector>
 
 using namespace std;
 
@@ -13,13 +14,24 @@ enum TNodeType {
 	NONE
 };
 
-class TNode
-{
+class TNode{
+
+private:
+	string name;
+	TNode* parentNode;
+	vector<TNode*> childNodes;	
+	vector<TNode*> siblingNodes;
+	TNodeType type;
+
 public:
 	TNode(void);
+	TNode(TNodeType,string);
 	~TNode(void);
 
-	static TNode createTNode(TNodeType nodeType, string name);
-	TNodeType getTType(TNode node);
+	TNodeType getTType();
 	bool isTTYpe(TNode node, TNodeType type);
+	bool addSibling(TNode siblingNode);
+	vector<TNode*> getSiblingsNodes();
+	TNode getNext();
+	bool setParent(TNode parentNode);
 };
