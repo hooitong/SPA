75ba97a1f1ab83b5e61a6236c50172584123e43d
Yeap Hooi Tong <hooitongx@gmail.com>
2015-03-22 21:22:08 +0800

    First Parent/Parent* Implementation
    Implemented the APIs within Parent class.

--- EmptyGeneralTesting/source/Parent.cpp
+++ EmptyGeneralTesting/source/Parent.cpp
@@ -1,36 +1,92 @@
 #include "Parent.h"
 
 
-Parent::Parent(void)
-{
-}
-
-
-bool setParent(STMTLINE parent, STMTLINE child){
-	return true;
-}
-bool isParent(STMTLINE first, STMTLINE second){
-	return true;
-}
-bool isParentStar(STMTLINE first, STMTLINE second){
-	return true;
-}
-TNode getParent(STMTLINE child){
-	return TNode();
-}
-vector<TNode> getParentStar(STMTLINE child){
-vector<TNode> a;
-	return a;
-}
-vector<TNode> getChildOf(STMTLINE parent){
-vector<TNode> a;
-	return a;
-}
-vector<TNode> getChildOfStar(STMTLINE parent){
-	vector<TNode> a;
-	return a;
+/* Constructor & Destructor */
+Parent::Parent(void){
 }
 
 Parent::~Parent(void)
 {
 }
+
+bool Parent::setParent(STMTLINE parent, STMTLINE child){
+	parent2ChildMap.insert(std::pair<STMTLINE, STMTLINE>(parent, child));
+	child2ParentMap.insert(child, parent);
+	parent2ChildMapStar.insert(std::pair<STMTLINE, STMTLINE>(parent, child));
+	child2ParentMapStar.insert(std::pair<STMTLINE, STMTLINE>(child, parent));
+	return true;
+}
+
+bool Parent::setParentStar(STMTLINE parent, STMTLINE child){
+	parent2ChildMapStar.insert(std::pair<STMTLINE, STMTLINE>(parent, child));
+	child2ParentMapStar.insert(std::pair<STMTLINE, STMTLINE>(child, parent));
+	return true;
+}
+
+bool Parent::isParent(STMTLINE first, STMTLINE second){
+	ret = parent2ChildMap.equal_range(first); 
+	/* If first is not a parent, return false */
+	if(ret.first == ret.second) {
+		return false;
+	} else {
+		for(it = ret.first; it != ret.second; ++it) {
+			if((*it).second == second) {
+				return true;
+			}
+		}
+		/* second is not found, return false */
+		return false;
+	}
+}
+bool Parent::isParentStar(STMTLINE first, STMTLINE second){
+	ret = parent2ChildMapStar.equal_range(first); 
+	/* If first is not a parent, return false */
+	if(ret.first == ret.second) {
+		return false;
+	} else {
+		for(it = ret.first; it != ret.second; ++it) {
+			if((*it).second == second) {
+				return true;
+			}
+		}
+		/* second is not found, return false */
+		return false;
+	}
+}
+
+STMTLINE Parent::getParent(STMTLINE child){
+	it2 = child2ParentMap.find(child);
+	if(it2 == child2ParentMap.end()){
+		return -1;
+	} else {
+		return it2->second;
+	}
+}
+
+vector<STMTLINE> Parent::getParentStar(STMTLINE child){
+	vector<STMTLINE> parentList;
+	ret = child2ParentMap.equal_range(child);
+	for(it = ret.first; it != ret.second; ++it) {
+		parentList.push_back((*it).second);
+	}
+	return parentList;
+}
+
+vector<STMTLINE> Parent::getChildOf(STMTLINE parent){
+	vector<STMTLINE> childList;
+	ret = parent2ChildMap.equal_range(parent);
+	for(it = ret.first; it != ret.second; ++it) {
+		childList.push_back((*it).second);
+	}
+	return childList;
+}
+
+vector<STMTLINE> Parent::getChildOfStar(STMTLINE parent){
+	vector<STMTLINE> childList;
+	ret = parent2ChildMapStar.equal_range(parent);
+	for(it = ret.first; it != ret.second; ++it) {
+		childList.push_back((*it).second);
+	}
+	return childList;
+}
+
--- EmptyGeneralTesting/source/Parent.h
+++ EmptyGeneralTesting/source/Parent.h
@@ -1,23 +1,30 @@
 #pragma once
-#include <vector>
+#include <map>
 #include "GlobalTypedef.h"
 #include "TNode.h"
 
 
 class Parent
 {
+private:
+	static std::multimap<STMTLINE, STMTLINE> parent2ChildMap;
+	static std::map<STMTLINE, STMTLINE> child2ParentMap;
+	static std::multimap<STMTLINE, STMTLINE> parent2ChildMapStar;
+	static std::multimap<STMTLINE, STMTLINE> child2ParentMapStar;
+	static std::multimap<STMTLINE,STMTLINE>::iterator it;
+	static std::map<STMTLINE,STMTLINE>::iterator it2;
+	static std::pair <std::multimap<STMTLINE,STMTLINE>::iterator, std::multimap<STMTLINE,STMTLINE>::iterator> ret;
+
 public:
 	Parent(void);
 	~Parent(void);
-
-	bool setParent(STMTLINE parent, STMTLINE child);
-	bool isParent(STMTLINE first, STMTLINE second);
-	bool isParentStar(STMTLINE first, STMTLINE second);
-	TNode getParent(STMTLINE child);
-	vector<TNode> getParentStar(STMTLINE child);
-	vector<TNode> getChildOf(STMTLINE parent);
-	vector<TNode> getChildOfStar(STMTLINE parent);
-
-
+	static bool setParent(STMTLINE parent, STMTLINE child);
+	static bool setParentStar(STMTLINE parent, STMTLINE child);
+	static bool isParent(STMTLINE first, STMTLINE second);
+	static bool isParentStar(STMTLINE first, STMTLINE second);
+	static STMTLINE getParent(STMTLINE child);
+	static vector<STMTLINE> getParentStar(STMTLINE child);
+	static vector<STMTLINE> getChildOf(STMTLINE parent);
+	static vector<STMTLINE> getChildOfStar(STMTLINE parent);
 };
 
