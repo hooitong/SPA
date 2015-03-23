a530c1d72002f4257db7e8f73036068682775748
Yeap Hooi Tong <hooitongx@gmail.com>
2015-03-23 09:57:32 +0800

    First Follows/Follows* Implementation

--- EmptyGeneralTesting/source/Follows.h
+++ EmptyGeneralTesting/source/Follows.h
@@ -1,21 +1,27 @@
 #pragma once
 #include <vector>
+#include <map>
 #include "GlobalTypedef.h"
 #include "TNode.h"
 
-using namespace std;
-
-
 class Follows
 {
+private:
+	static std::map<STMTLINE, STMTLINE> rightSiblingMap;
+	static std::map<STMTLINE, STMTLINE> leftSiblingMap;
+	static std::map<STMTLINE,STMTLINE>::iterator it;
+
 public:
 	Follows(void);
 	~Follows(void);
 
-	TNode setFollows(STMTLINE STMTLINEFirst, STMTLINE STMTLINESecond);
-	bool isFollow(STMTLINE STMTLINEFirst, STMTLINE STMTLINESecond);
-	bool isFollowStar(STMTLINE STMTLINEFirst, STMTLINE STMTLINESecond);
-	vector<TNode> getFollowedByStar(STMTLINE currentLine);
-	vector<TNode> getFollowsFromStar(STMTLINE currentLine);
+	static bool setFollows(STMTLINE first, STMTLINE second);
+	static bool setFollowsStar(STMTLINE first, STMTLINE second);
+	static bool isFollow(STMTLINE first, STMTLINE second);
+	static bool isFollowStar(STMTLINE first, STMTLINE second);
+	static STMTLINE getFollowedBy(STMTLINE currentLine);
+	static STMTLINE getFollowsFrom(STMTLINE currentLine);
+	static vector<STMTLINE> getFollowedByStar(STMTLINE currentLine);
+	static vector<STMTLINE> getFollowsFromStar(STMTLINE currentLine);
 };
 
