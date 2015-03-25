4c9a53fd183d4e7d3699c79436c326977aa5a44b
Yeap Hooi Tong <hooitongx@gmail.com>
2015-03-25 20:01:04 +0800

    Updated Method Stubs on QueryEvaluator

--- EmptyGeneralTesting/source/QueryEvaluator.cpp
+++ EmptyGeneralTesting/source/QueryEvaluator.cpp
@@ -4,18 +4,40 @@ string QueryEvaluator::evaluate(QueryTree* query) {
     return "Incomplete Stub Method";
 }
 
-vector<std::pair<STMTLINE, STMTLINE>> Follows(TType firstType, TType secondType) {
+vector<std::pair<STMTLINE, STMTLINE>> QueryEvaluator::Follows(TType firstType, TType secondType) {
+	// retrieve all stmt# from AST that belongs to the firstType
+		
+
+	// retrieve all stmt# from AST that belongs to the secondType
+
+
+	// double for loop based on both vector of stmt#, check Follows is true then keep into vector as tuple.
+
 
 }
 
-vector<std::pair<STMTLINE, STMTLINE>> Parent(TType firstType, TType secondType) {
+vector<std::pair<STMTLINE, STMTLINE>> QueryEvaluator::Parent(TType firstType, TType secondType) {
+	// retrieve all stmt# from AST that belongs to the firstType
 
+	// retrieve all stmt# from AST that belongs to the secondType
+
+	// double for loop based on both vector of stmt#, check Follows is true then keep into vector as tuple.
+
+	// return the vector of tuples
 }
 
-vector<std::pair<STMTLINE, STMTLINE>> Modifies(TType firstType, TType secondType) {
+vector<std::pair<STMTLINE, STMTLINE>> QueryEvaluator::Modifies(TType firstType) {
+	// retrieve all stmt# from AST that belongs to the firstType
 
+	// for each stmt#, store all combination tuple of the # and var that it modifies
+
+	// return the vector of tuples
 }
 
-vector<std::pair<STMTLINE, STMTLINE>> Uses(TType firstType, TType secondType) {
+vector<std::pair<STMTLINE, STMTLINE>> QueryEvaluator::Uses(TType firstType) {
+	// retrieve all stmt# from AST that belongs to the firstType
 
+	// for each stmt#, store all combination tuple of the # and var that it uses
+
+	// return the vector of tuples
 }
--- EmptyGeneralTesting/source/QueryEvaluator.h
+++ EmptyGeneralTesting/source/QueryEvaluator.h
@@ -1,8 +1,9 @@
 #pragma once
 
-#include "QueryTree.h"
-
 #include <string>
+
+#include "PKB.h"
+#include "QueryTree.h"
 
 
 class QueryEvaluator {
@@ -11,8 +12,9 @@ class QueryEvaluator {
 
 // Handle evaluation with wildcards of a certain type. (May be relocated in the future)
   private:
+    PKB pkbInstance; //Stub instance for coding. Need to have instance accessible by Query System.
     vector<std::pair<STMTLINE, STMTLINE>> Follows(TType firstType, TType secondType);
     vector<std::pair<STMTLINE, STMTLINE>> Parent(TType firstType, TType secondType);
-    vector<std::pair<STMTLINE, STMTLINE>> Modifies(TType firstType, TType secondType);
-    vector<std::pair<STMTLINE, STMTLINE>> Uses(TType firstType, TType secondType);
+    vector<std::pair<STMTLINE, STMTLINE>> Modifies(TType firstType); // get all tuples of the type + var that it modifies
+    vector<std::pair<STMTLINE, STMTLINE>> Uses(TType firstType); // get all tuples of the type + var that it uses
 };
