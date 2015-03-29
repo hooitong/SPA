2a35c915379f1716aeb7210d4b5cc59b9dd17ecb
Jason Sia <swkjason@hotmail.com>
2015-03-29 19:51:37 +0800

    Add ProcTableTest Stubs

---
+++ EmptyGeneralTesting/UnitTesting/source/ProcTableTest.cpp
@@ -0,0 +1,51 @@
+#include <cppunit/config/SourcePrefix.h>
+#include "ProcTable.h"
+#include "ProcTableTest.h"
+
+	
+void ProcTableTest::setUp() {
+	procTable = new ProcTable;
+}
+
+void ProcTableTest::tearDown() {
+	delete procTable;
+}
+
+CPPUNIT_TEST_SUITE_REGISTRATION(ProcTableTest);
+
+void ProcTableTest:: testInsertProc(){
+
+};
+void ProcTableTest:: testGetProcIndex(){
+
+};
+void ProcTableTest:: testGetProcName(){
+
+};
+void ProcTableTest:: testGetAllProc(){
+
+};
+void ProcTableTest:: testSetTRoot(){
+
+};
+void ProcTableTest:: testSetGRoot(){
+
+};
+void ProcTableTest:: testGetTRoot(){
+
+};
+void ProcTableTest:: testGetGNode(){
+
+};
+void ProcTableTest:: testAddModified(){
+
+};
+void ProcTableTest:: testAddUsed(){
+
+};
+void ProcTableTest:: testGetModified(){
+
+};
+void ProcTableTest:: testGetUsed(){
+
+};
