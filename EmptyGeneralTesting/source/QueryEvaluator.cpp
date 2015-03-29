a51143298d61a5a574c44258d8f0f9c58a1af936
nathanajah <nathanajah@yahoo.com>
2015-03-29 13:31:23 +0800

    Added evaluator support for Follows

--- EmptyGeneralTesting/source/QueryEvaluator.cpp
+++ EmptyGeneralTesting/source/QueryEvaluator.cpp
@@ -1,7 +1,143 @@
 #include "QueryEvaluator.h"
 
-string QueryEvaluator::evaluate(QueryTree* query) {
+string QueryEvaluator::evaluate(QueryTree* tree) {
     return "Incomplete Stub Method";
+}
+
+QueryResult QueryEvaluator::evaluate(QNode* node) {
+	if (node->getQType() == SUCHTHATLIST ||
+		node->getQType() == WITHLIST ||
+		node->getQType() == PATTERNLIST) {
+		vector <string> emptySynonym;
+		QueryResult result =QueryResult(emptySynonym);
+
+		vector<QNode*> children = node->getChildren();
+		for (int i = 0; i < children.size(); i++) {
+			result = result.merge(evaluate(children[i]));
+		}
+
+		return result;
+	} else if (node->getQType() == RELATION) {
+		return solveRelation(node);
+	}
+}
+
+QueryResult QueryEvaluator::solveRelation(QNode* node) {
+	assert(node->getQType() == RELATION);
+	if (node->getString() == "Follows") {
+		return solveFollows(node);
+	}
+}
+
+
+QueryResult QueryEvaluator::solveFollows(QNode* node) {
+	assert(node->getQType() == RELATION && node->getString() == "Follows");
+	QNode* leftChild = node->getChildren()[0];
+	QNode* rightChild = node->getChildren()[1];
+	if (isSynonym(leftChild->getQType()) && isSynonym(rightChild->getQType())) {
+		TType type1 = synonymToTType(leftChild->getQType());
+		TType type2 = synonymToTType(rightChild->getQType());
+		return pairsToResult(Follows(type1, type2), 
+			leftChild->getString(), rightChild->getString());
+	} else if (isSynonym(leftChild->getQType())) {
+		int line = getInteger(rightChild);
+		int resultLine = pkbInstance.getFollows()->getFollowsFrom(line);
+		return oneToResult(resultLine, leftChild->getString());
+	} else if (isSynonym(rightChild->getQType())) {
+		int line = getInteger(leftChild);
+		int resultLine = pkbInstance.getFollows()->getFollowedBy(line);
+		return oneToResult(resultLine, leftChild->getString());
+	} else {
+		int line1 = getInteger(leftChild);
+		int line2 = getInteger(rightChild);
+		return boolToResult(pkbInstance.getFollows()->isFollow(line1, line2));
+	}
+}
+
+int QueryEvaluator::getInteger(QNode* node) {
+	istringstream iss(node->getString());
+	int result;
+	iss >> result;
+	return result;
+}
+
+TType QueryEvaluator::synonymToTType(QNodeType type) {
+	if (type == WHILESYNONYM) {
+		return WHILEN;
+	} else if (type == ASSIGNSYNONYM) {
+		return ASSIGNN;
+	} else if (type == VARIABLESYNONYM) {
+		return VARN;
+	} else if (type == PROCEDURESYNONYM) {
+		return PROCEDUREN;
+	}
+}
+
+bool QueryEvaluator::isSynonym(QNodeType type) {
+	return type == WHILESYNONYM ||
+		type == ASSIGNSYNONYM ||
+		type == VARIABLESYNONYM ||
+		type == PROCEDURESYNONYM ||
+		type == CONSTSYNONYM ||
+		type == PROGLINESYNONYM ||
+		type == IFSYNONYM;
+}
+
+QueryResult QueryEvaluator::boolToResult(bool boolResult) {
+	vector <string> synonyms;
+	QueryResult result = QueryResult(synonyms);
+
+	if (boolResult) {
+		result.addSolution(vector<int>());
+	}
+	return result;
+}
+
+QueryResult QueryEvaluator::pairsToResult(vector<pair<int,int> > pairs, string synonym1, string synonym2) {
+	vector <string> synonyms;
+	synonyms.push_back(synonym1);
+	synonyms.push_back(synonym2);
+	QueryResult result = QueryResult(synonyms);
+
+	for (int i = 0; i < (int) pairs.size(); i++) {
+		vector <int> resultTuple;
+		resultTuple.push_back(pairs[i].first);
+		resultTuple.push_back(pairs[i].second);
+		result.addSolution(resultTuple);
+	}
+	return result;
+}
+
+QueryResult QueryEvaluator::oneToResult(int oneresult, string synonym) {
+	vector <int> singles;
+	singles.push_back(oneresult);
+
+	return singlesToResult(singles, synonym);
+}
+
+QueryResult QueryEvaluator::singlesToResult(vector<int> singles, string synonym) {
+	vector<string> synonyms;
+	synonyms.push_back(synonym);
+	QueryResult result(synonyms);
+	for (int i = 0; i < (int)singles.size(); i++) {
+		vector<int> tuple;
+		tuple.push_back(singles[i]);
+		result.addSolution(tuple);
+	}
+	return result;	
+}
+
+QueryResult QueryEvaluator::getAllOfType(TType t, string synonym) {
+	vector <int> possibleResult;
+	if (t == WHILEN || t == ASSIGNN) {
+		possibleResult = pkbInstance.getAst()->getStmtLines(t);
+	} else if (t == PROCEDUREN) {
+		//TODO
+	} else if (t == VARN) {
+		possibleResult = pkbInstance.getVarTable()->getAllVarIndex();
+	}
+
+	return singlesToResult(possibleResult, synonym);
 }
 
 vector<std::pair<STMTLINE, STMTLINE>> QueryEvaluator::Follows(TType firstType, TType secondType) {
--- EmptyGeneralTesting/source/QueryEvaluator.h
+++ EmptyGeneralTesting/source/QueryEvaluator.h
@@ -2,9 +2,12 @@
 
 #include <string>
 #include <vector>
+#include <cassert>
+#include <sstream>
 
 #include "PKB.h"
 #include "QueryTree.h"
+#include "QueryResult.h"
 
 
 class QueryEvaluator {
@@ -14,8 +17,20 @@ class QueryEvaluator {
 // Handle evaluation with wildcards of a certain type. (May be relocated in the future)
   private:
     PKB pkbInstance; //Stub instance for coding. Need to have instance accessible by Query System.
+	QueryResult evaluate(QNode* node);
     vector<std::pair<STMTLINE, STMTLINE>> Follows(TType firstType, TType secondType);
     vector<std::pair<STMTLINE, STMTLINE>> Parent(TType firstType, TType secondType);
     vector<std::pair<STMTLINE, VARINDEX>> Modifies(TType firstType); // get all tuples of the type + var that it modifies
     vector<std::pair<STMTLINE, VARINDEX>> Uses(TType firstType); // get all tuples of the type + var that it uses
+	TType synonymToTType(QNodeType type);
+	int getInteger(QNode* node);
+	QueryResult solveRelation(QNode* node);
+	QueryResult solveFollows(QNode* node);
+	QueryResult solveParent(QNode* node);
+	bool isSynonym(QNodeType type);
+	QueryResult pairsToResult(vector<pair<int,int> > pairs, string synonym1, string synonym2);
+	QueryResult singlesToResult(vector<int> singles, string synonym);
+	QueryResult oneToResult(int oneResult, string synonym);
+	QueryResult boolToResult(bool boolResult);
+	QueryResult getAllOfType(TType t, string synonym);
 };
