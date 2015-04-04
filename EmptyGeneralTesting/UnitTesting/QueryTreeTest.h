256860379987864bcd81cfd24b659c3873a55a33
jonathanirvings <jonathan.irvin@yahoo.com>
2015-04-04 14:17:42 +0800

    Added tests for QueryTree (and QNode)

---
+++ EmptyGeneralTesting/UnitTesting/QueryTreeTest.h
@@ -0,0 +1,22 @@
+#pragma once
+
+#include <cppunit/extensions/HelperMacros.h>
+
+#include "QueryTree.h"
+
+class QueryTreeTest : public CPPUNIT_NS::TestFixture {
+    CPPUNIT_TEST_SUITE(QueryTreeTest);
+    CPPUNIT_TEST(testCreateQNode);
+	CPPUNIT_TEST(testSetAsRoot);
+	CPPUNIT_TEST(testAddChild);
+	CPPUNIT_TEST_SUITE_END();
+
+  private:
+    QueryTree* queryTree;
+
+  public:
+	QueryTreeTest();
+    void testCreateQNode();
+	void testSetAsRoot();
+	void testAddChild();
+};
