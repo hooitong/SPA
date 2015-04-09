cf39473ab2fb6617665aa9f04808c581bd9630be
Yeap Hooi Tong <hooitongx@gmail.com>
2015-04-10 07:49:17 +0800

    Updated DesignExtractor Modifies/Uses
    Passed Integration Test from Parser to PKB after Changes.

--- EmptyGeneralTesting/source/DesignExtractor.cpp
+++ EmptyGeneralTesting/source/DesignExtractor.cpp
@@ -43,8 +43,15 @@ void DesignExtractor::extractParentStar() {
     Parent* p = (*pkbObj).getParent();
     STMTLINE root = 1;
 
+    // extract the first statement
     vector<STMTLINE> parents;
     recursiveExtractParent(p, root, parents);
+
+    // for each sibling of the root, call the extraction
+    vector<STMTLINE> rightRootSiblings = pkbObj->getFollows()->getFollowedByStar(root);
+    for (std::vector<int>::iterator it = rightRootSiblings.begin(); it != rightRootSiblings.end(); ++it) {
+        recursiveExtractParent(p, *it, parents);
+    }
 }
 
 // recursive method which starts from the root of the AST/Procedure and based on the parent mapping to
@@ -73,21 +80,22 @@ void DesignExtractor::extractModifiesContainer() {
     vector<STMTLINE> aList = pkbObj->getAst()->getStmtLines(ASSIGNN);
     for(std::vector<STMTLINE>::iterator i = aList.begin(); i != aList.end(); ++i) {
         vector<STMTLINE> pList = pkbObj->getParent()->getParentStar(*i);
-		vector<VARINDEX> vList = pkbObj->getModifies()->getModifiedByStmt(*i);
+        vector<VARINDEX> vList = pkbObj->getModifies()->getModifiedByStmt(*i);
         for(std::vector<VARINDEX>::iterator k = vList.begin(); k != vList.end(); ++k) {
-			for(std::vector<STMTLINE>::iterator j = pList.begin(); j != pList.end(); ++j) {
-				if(!isModifiesDuplicate(*j, *k)) {
-					pkbObj->getModifies()->setModifiesStmt(*k, *j);
-				}
-			}
+            for(std::vector<STMTLINE>::iterator j = pList.begin(); j != pList.end(); ++j) {
+                if(!isModifiesDuplicate(*j, *k)) {
+                    pkbObj->getModifies()->setModifiesStmt(*k, *j);
+                }
+            }
         }
     }
 }
 
 void DesignExtractor::extractUsesContainer() {
 	PKB* pkbObj = PKB::getPKB();
-	vector<STMTLINE> aList = pkbObj->getAst()->getStmtLines(ASSIGNN);
-	for(std::vector<STMTLINE>::iterator i = aList.begin(); i != aList.end(); ++i) {
+
+	vector<STMTLINE> cList = pkbObj->getAst()->getStmtLines(WHILEN);
+	for(std::vector<STMTLINE>::iterator i = cList.begin(); i != cList.end(); ++i) {
 		vector<STMTLINE> pList = pkbObj->getParent()->getParentStar(*i);
 		vector<VARINDEX> vList = pkbObj->getUses()->getUsedByStmt(*i);
 		for(std::vector<VARINDEX>::iterator k = vList.begin(); k != vList.end(); ++k) {
@@ -98,16 +106,29 @@ void DesignExtractor::extractUsesContainer() {
 			}
 		}
 	}
+
+    vector<STMTLINE> aList = pkbObj->getAst()->getStmtLines(ASSIGNN);
+    for(std::vector<STMTLINE>::iterator i = aList.begin(); i != aList.end(); ++i) {
+        vector<STMTLINE> pList = pkbObj->getParent()->getParentStar(*i);
+        vector<VARINDEX> vList = pkbObj->getUses()->getUsedByStmt(*i);
+        for(std::vector<VARINDEX>::iterator k = vList.begin(); k != vList.end(); ++k) {
+            for(std::vector<STMTLINE>::iterator j = pList.begin(); j != pList.end(); ++j) {
+                if(!isUsesDuplicate(*j, *k)) {
+                    pkbObj->getUses()->setUsesStmt(*k, *j);
+                }
+            }
+        }
+    }
 }
 
 bool DesignExtractor::isModifiesDuplicate(STMTLINE source, VARINDEX item) {
-	Modifies* mObj = PKB::getPKB()->getModifies();
-	vector<VARINDEX> list = mObj->getModifiedByStmt(source);
-	return find(list.begin(), list.end(), item) != list.end();
+    Modifies* mObj = PKB::getPKB()->getModifies();
+    vector<VARINDEX> list = mObj->getModifiedByStmt(source);
+    return find(list.begin(), list.end(), item) != list.end();
 }
 
 bool DesignExtractor::isUsesDuplicate(STMTLINE source, VARINDEX item) {
-	Uses* uObj = PKB::getPKB()->getUses();
-	vector<VARINDEX> list = uObj->getUsedByStmt(source);
-	return find(list.begin(), list.end(), item) != list.end();
+    Uses* uObj = PKB::getPKB()->getUses();
+    vector<VARINDEX> list = uObj->getUsedByStmt(source);
+    return find(list.begin(), list.end(), item) != list.end();
 }
