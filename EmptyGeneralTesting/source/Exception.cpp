bb5b293ec4801928ca4e3c44831db4551a38f280
The Nguyen <nguyentheimo2011@gmail.com>
2015-04-14 14:35:28 +0800

    fix bug in printing out message for exception

--- EmptyGeneralTesting/source/Exception.cpp
+++ EmptyGeneralTesting/source/Exception.cpp
@@ -36,7 +36,7 @@ SyntaxErrorException::~SyntaxErrorException() {
 
 string SyntaxErrorException::message() {
 	string s = std::to_string(static_cast<long long>(lineIndex));
-	string message = "Invalid name error at line ";
+	string message = "Syntax error at line ";
 	return message.append(s);
 }
 
