459739560c0a57eceeeb5b74c54afd3e7bd3516e
Hooi Tong <a0111736@u.nus.edu>
2015-09-07 19:29:26 +0800

    Implemented `Calls` in PKB

--- EmptyGeneralTesting/source/Calls.h
+++ EmptyGeneralTesting/source/Calls.h
@@ -1,6 +1,24 @@
 ﻿#pragma once
 
+#include "BitTable.h"
+#include "GlobalType.h"
 class Calls {
-public:
-	
+	private:
+		BitTable callerToCalled;
+		BitTable calledToCaller;
+		BitTable callerToCalledStar;
+		BitTable calledToCallerStar;
+
+	public:
+		Calls(void);
+		~Calls(void);
+		
+		void setCalls(PROCINDEX caller, PROCINDEX called);
+		void setCallsStar(PROCINDEX caller, PROCINDEX called);
+		bool isCall(PROCINDEX caller, PROCINDEX called);
+		bool isCallStar(PROCINDEX caller, PROCINDEX called);
+		vector<PROCINDEX> getCalledBy(PROCINDEX caller);
+		vector<PROCINDEX> getCalledByStar(PROCINDEX caller);
+		vector<PROCINDEX> getCallsTo(PROCINDEX called);
+		vector<PROCINDEX> getCallsToStar(PROCINDEX called);
 };
