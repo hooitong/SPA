487716a601962d25efd1b755eed57074330a4011
Hooi Tong <a0111736@u.nus.edu>
2015-09-07 17:59:31 +0800

    Shifted `QueryTable` file location to `source` and included Calls stub file

--- EmptyGeneralTesting/source/Calls.h
+++ EmptyGeneralTesting/source/Calls.h
@@ -1,22 +1,6 @@
-#pragma once
-
-#include <vector>
-#include <map>
-
-#include "GlobalType.h"
+﻿#pragma once
 
 class Calls {
-  private:
-	std::multimap<PROCINDEX,PROCINDEX> procCalls;
-    std::multimap<PROCINDEX,PROCINDEX>::iterator procIt;//need to check
-    std::pair <std::multimap<PROCINDEX,PROCINDEX>::iterator, std::multimap<PROCINDEX,PROCINDEX>::iterator> procRet;//need to check
-
-  public:
-    Calls(void);
-    ~Calls(void);
-
-	void setProcCalls(PROCINDEX procIndexCalls, PROCINDEX procIndexCalled);
-    vector<VARINDEX> getCalledByProc(PROCINDEX procIndex);
-    vector<STMTLINE> getCalls(PROCINDEX procIndex);
-
+public:
+	
 };
