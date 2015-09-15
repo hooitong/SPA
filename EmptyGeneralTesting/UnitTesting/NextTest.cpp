1b1751e007dabc9a4adcd612c86407e414c6c42b
Hooi Tong <hooitongx@gmail.com>
2015-09-15 20:32:05 +0800

    Added unit testing stub for `Next`

---
+++ EmptyGeneralTesting/UnitTesting/NextTest.cpp
@@ -0,0 +1,29 @@
+#include "NextTest.h"
+
+void NextTest::setUp() {
+  Next = new Next;
+}
+
+void NextTest::tearDown() {
+}
+
+void NextTest::testSetNext() {
+}
+
+void NextTest::testSetNextStar() {
+}
+
+void NextTest::testIsNext() {
+}
+
+void NextTest::testGetBefore() {
+}
+
+void NextTest::testGetBeforeStar() {
+}
+
+void NextTest::testGetNext() {
+}
+
+void NextTest::testGetNextStar() {
+}
