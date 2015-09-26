146367523cf286e47d615f1063cd36636ef4a5bc
Hooi Tong <hooitongx@gmail.com>
2015-09-26 21:59:41 +0800

    Fixed bug in `CFG` where `procToRoot` is mapped wrongly

--- EmptyGeneralTesting/source/CFG.cpp
+++ EmptyGeneralTesting/source/CFG.cpp
@@ -3,64 +3,64 @@
 
 /* CFG Class Implementation */
 void CFG::setProcToRoot(PROCINDEX proc, GNode* root) {
-	procToRoot[proc] = root;
+  procToRoot[proc] = root;
 }
 
 void CFG::setLineToNode(PROGLINE lineNumber, GNode* node) {
-	lineToNode[lineNumber] = node;
+  lineToNode[lineNumber] = node;
 }
 
 vector<GNode*> CFG::getAllRootNodes() {
-	vector<GNode*> allCFG;
-	std::map<PROCINDEX, GNode*>::iterator it;
-	for (it = procToRoot.begin(); it != procToRoot.end(); ++it) {
-		allCFG.push_back(it->second);
-	}
-	return allCFG;
+  vector<GNode*> allCFG;
+  std::map<PROCINDEX, GNode*>::iterator it;
+  for (it = procToRoot.begin(); it != procToRoot.end(); ++it) {
+    allCFG.push_back(it->second);
+  }
+  return allCFG;
 }
 
 
-void CFG::insert(STMTLINE from, STMTLINE to, PROCINDEX procIndex){
-	GNode* nodeFrom;
-	GNode* nodeTo;
-		
-	if ( lineToNode.find(from) == lineToNode.end() ) {
-		//not found
-		nodeFrom = new GNode(from);
-		setLineToNode(from, nodeFrom);
-	}
-	else{
-		nodeFrom = getGNode(from);
-	}
+void CFG::insert(STMTLINE from, STMTLINE to, PROCINDEX procIndex) {
+  GNode* nodeFrom;
+  GNode* nodeTo;
 
-	if ( lineToNode.find(to) == lineToNode.end() ) {
-		//not found
-		nodeTo = new GNode(to);
-		setLineToNode(to, nodeTo);
-	}
-	else{
-		nodeTo = getGNode(to);
-	}
+  if (lineToNode.find(from) == lineToNode.end()) {
+    //not found
+    nodeFrom = new GNode(from);
+    setLineToNode(from, nodeFrom);
+  }
+  else {
+    nodeFrom = getGNode(from);
+  }
 
-	if ( procToRoot.find(from) == procToRoot.end() ) {
-		//not found
-	    setProcToRoot(from, nodeFrom); 
-	}
-	
-	nodeFrom->addForwardNode(nodeTo);
-	nodeTo->addPrevNode(nodeFrom);
-	
+  if (lineToNode.find(to) == lineToNode.end()) {
+    //not found
+    nodeTo = new GNode(to);
+    setLineToNode(to, nodeTo);
+  }
+  else {
+    nodeTo = getGNode(to);
+  }
+
+  if (procToRoot.find(procIndex) == procToRoot.end()) {
+    //not found
+    setProcToRoot(procIndex, nodeFrom);
+  }
+
+  nodeFrom->addForwardNode(nodeTo);
+  nodeTo->addPrevNode(nodeFrom);
+
 }
 
 GNode* CFG::getGNode(PROGLINE line) {
-	return lineToNode[line];
+  return lineToNode[line];
 }
 
-vector<GNode*> CFG::getAllGNodes(){
-	vector<GNode*> allGNodes;
-	std::map<STMTLINE, GNode*>::iterator it;
-	for (it = lineToNode.begin(); it != lineToNode.end(); ++it) {
-		allGNodes.push_back(it->second);
-	}
-	return allGNodes;
+vector<GNode*> CFG::getAllGNodes() {
+  vector<GNode*> allGNodes;
+  std::map<STMTLINE, GNode*>::iterator it;
+  for (it = lineToNode.begin(); it != lineToNode.end(); ++it) {
+    allGNodes.push_back(it->second);
+  }
+  return allGNodes;
 }
