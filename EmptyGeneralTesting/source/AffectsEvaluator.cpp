18dbb2259c3d85182fda1557b41d2f65d4048208
Hooi Tong <hooitongx@gmail.com>
2015-10-06 21:49:03 +0800

    Lazy implementation of `Affects` for all query combinations

--- EmptyGeneralTesting/source/AffectsEvaluator.cpp
+++ EmptyGeneralTesting/source/AffectsEvaluator.cpp
@@ -7,7 +7,51 @@
 
 AffectsEvaluator::AffectsEvaluator(PKB* pkb) : RelationEvaluator(pkb) {}
 
-bool AffectsEvaluator::solveConstConst(const int left, const int right, const QueryResult& result) const {
+QueryResult AffectsEvaluator::evaluate(QNode* node, const QueryResult& result) {
+  QNode* leftChild = node->getChildren()[0];
+  QNode* rightChild = node->getChildren()[1];
+
+  if (leftChild->getQType() == ANY &&
+    rightChild->getQType() == ANY) {
+    return QueryResult(solveAnyAny(result));
+  }
+  else if (leftChild->getQType() == ANY &&
+    isSynonym(rightChild->getQType())) {
+    return QueryResult(solveAnySyn(result), rightChild->getString());
+  }
+  else if (leftChild->getQType() == ANY &&
+    rightChild->getQType() == CONST) {
+    return QueryResult(solveAnyConst(getInteger(leftChild), result));
+  }
+  else if (isSynonym(leftChild->getQType()) &&
+    rightChild->getQType() == ANY) {
+    return QueryResult(solveSynAny(result), leftChild->getString());
+  }
+  else if (isSynonym(leftChild->getQType()) &&
+    isSynonym(rightChild->getQType())) {
+    return QueryResult(solveSynSyn(result), leftChild->getString(), rightChild->getString());
+  }
+  else if (isSynonym(leftChild->getQType()) &&
+    rightChild->getQType() == CONST) {
+    return QueryResult(solveSynConst(getInteger(rightChild), result), leftChild->getString());
+  }
+  else if (leftChild->getQType() == CONST &&
+    rightChild->getQType() == ANY) {
+    return QueryResult(solveConstAny(getInteger(leftChild), result));
+  }
+  else if (leftChild->getQType() == CONST &&
+    isSynonym(rightChild->getQType())) {
+    return QueryResult(solveConstSyn(getInteger(leftChild), result), rightChild->getString());
+  }
+  else if (leftChild->getQType() == CONST &&
+    rightChild->getQType() == CONST) {
+    return QueryResult(solveConstConst(getInteger(leftChild), getInteger(rightChild), result));
+  }
+}
+
+
+bool AffectsEvaluator::solveConstConst(const int left, const int right, const QueryResult& result) {
+  PKB *pkb = PKB::getPKB();
   AST *ast = pkb->getAst();
 
   /* Left and Right are Statement Lines */
@@ -30,7 +74,9 @@ bool AffectsEvaluator::solveConstConst(const int left, const int right, const Qu
   return isTypeCorrect && isSameContext && isContainsPath && isNotModifiedBetween;
 }
 
-vector<int> AffectsEvaluator::solveConstSyn(const int left, const QueryResult& result) const {
+vector<int> AffectsEvaluator::solveConstSyn(const int left, const QueryResult& result) {
+  PKB *pkb = PKB::getPKB();
+
   /* Retrieve all statements reached by left */
   vector<STMTLINE> possibleRight = pkb->getNext()->getNextStar(left);
 
@@ -46,7 +92,9 @@ vector<int> AffectsEvaluator::solveConstSyn(const int left, const QueryResult& r
   return resultRight;
 }
 
-vector<int> AffectsEvaluator::solveSynConst(const int right, const QueryResult& result) const {
+vector<int> AffectsEvaluator::solveSynConst(const int right, const QueryResult& result) {
+  PKB *pkb = PKB::getPKB();
+
   /* Retrieve all statements that can reach to right */
   vector<STMTLINE> possibleLeft = pkb->getNext()->getBeforeStar(right);
 
@@ -62,33 +110,114 @@ vector<int> AffectsEvaluator::solveSynConst(const int right, const QueryResult& 
   return resultLeft;
 }
 
-int AffectsEvaluator::getConstLeft(const QNode* const node) const {
-  return true;
+vector<std::pair<int, int>> AffectsEvaluator::solveSynSyn(const QueryResult& result) {
+  PKB *pkb = PKB::getPKB();
+
+  /* Retrieve all assignment statement numbers in the program */
+  vector<int> allAssign = pkb->getAst()->getStmtLines(ASSIGNN);
+
+  /* Terrible O(N^3) performance loops - Lazy Implementation */
+  vector<std::pair<int, int>>  tupleResults;
+  for (int i = 0; i < allAssign.size(); i++) {
+    for (int j = 0; j < allAssign.size(); j++) {
+      if (i == j) continue;
+      if (solveConstConst(allAssign[i], allAssign[j], result))
+        tupleResults.push_back(make_pair(i, j));
+    }
+  }
+
+  return tupleResults;
 }
 
-int AffectsEvaluator::getConstRight(const QNode* const node) const {
-  return true;
+vector<int> AffectsEvaluator::solveAnySyn(const QueryResult& result) {
+  PKB *pkb = PKB::getPKB();
+
+  /* Retrieve all assignment statement numbers in the program */
+  vector<int> allAssign = pkb->getAst()->getStmtLines(ASSIGNN);
+
+  /* Terrible O(N^3) performance loops - Lazy Implementation */
+  vector<int>  rightResults;
+  for (int i = 0; i < allAssign.size(); i++) {
+    for (int j = 0; j < allAssign.size(); j++) {
+      if (i == j) continue;
+      if (solveConstConst(allAssign[i], allAssign[j], result))
+        rightResults.push_back(j);
+    }
+  }
+
+  return rightResults;
 }
 
-bool AffectsEvaluator::checkLeft(const QNode* const node, const int tested) const {
-  return true;
+vector<int> AffectsEvaluator::solveSynAny(const QueryResult& result) {
+  PKB *pkb = PKB::getPKB();
+
+  /* Retrieve all assignment statement numbers in the program */
+  vector<int> allAssign = pkb->getAst()->getStmtLines(ASSIGNN);
+
+  /* Terrible O(N^3) performance loops - Lazy Implementation */
+  vector<int>  leftResult;
+  for (int i = 0; i < allAssign.size(); i++) {
+    for (int j = 0; j < allAssign.size(); j++) {
+      if (i == j) continue;
+      if (solveConstConst(allAssign[i], allAssign[j], result))
+        leftResult.push_back(i);
+    }
+  }
+
+  return leftResult;
 }
 
-bool AffectsEvaluator::checkRight(const QNode* const node, const int tested) const {
-  return true;
+bool AffectsEvaluator::solveAnyConst(const int right, const QueryResult& result) {
+  PKB *pkb = PKB::getPKB();
+
+  /* Retrieve all assignment statement numbers in the program */
+  vector<int> allAssign = pkb->getAst()->getStmtLines(ASSIGNN);
+
+  /* Terrible O(N^2) performance loops - Lazy Implementation */
+  for (int i = 0; i < allAssign.size(); i++) {
+    return solveConstConst(allAssign[i], right, result);
+  }
+
+  return false;
 }
 
-vector<int> AffectsEvaluator::getAllLeft(const QNode* const node, const QueryResult& result) const {
-  vector<int> temp;
-  return temp;
+bool AffectsEvaluator::solveConstAny(const int left, const QueryResult& result) {
+  PKB *pkb = PKB::getPKB();
+
+  /* Retrieve all assignment statement numbers in the program */
+  vector<int> allAssign = pkb->getAst()->getStmtLines(ASSIGNN);
+
+  /* Terrible O(N^2) performance loops - Lazy Implementation */
+  vector<int>  leftResult;
+  for (int i = 0; i < allAssign.size(); i++) {
+    return solveConstConst(left, allAssign[i], result);
+  }
+
+  return false;
 }
 
-vector<int> AffectsEvaluator::getAllRight(const QNode* const node, const QueryResult& result) const {
-  vector<int> temp;
-  return temp;
+bool AffectsEvaluator::solveAnyAny(const QueryResult& result) {
+  PKB *pkb = PKB::getPKB();
+
+  /* Retrieve all assignment statement numbers in the program */
+  vector<int> allAssign = pkb->getAst()->getStmtLines(ASSIGNN);
+
+  /* Terrible O(N^3) performance loops - Lazy Implementation */
+  vector<int>  leftResult;
+  for (int i = 0; i < allAssign.size(); i++) {
+    for (int j = 0; j < allAssign.size(); j++) {
+      if (i == j) continue;
+      if (solveConstConst(allAssign[i], allAssign[j], result))
+        return true;
+    }
+  }
+
+  return false;
 }
 
-bool AffectsEvaluator::findPathToNode(STMTLINE current, STMTLINE end, VARINDEX contextVar, vector<STMTLINE> path) const {
+bool AffectsEvaluator::findPathToNode(STMTLINE current, STMTLINE end, VARINDEX contextVar, vector<STMTLINE> path) {
+  PKB *pkb = PKB::getPKB();
+
   /* Base Case */
   if (current == end) return true;
 
--- EmptyGeneralTesting/source/AffectsEvaluator.h
+++ EmptyGeneralTesting/source/AffectsEvaluator.h
@@ -3,22 +3,25 @@
 #include <PKB.h>
 #include <QueryResult.h>
 #include <QNode.h>
-#include <RelationEvaluator.h>
+#include "RelationEvaluator.h"
 
 class AffectsEvaluator : public RelationEvaluator {
 public:
   AffectsEvaluator(PKB* pkb);
+  QueryResult evaluate(QNode* node, const QueryResult& result);
 
 private:
-  bool solveConstConst(const int left, const int right, const QueryResult& result) const;
-  vector<int> solveConstSyn(const int left, const QueryResult& result) const;
-  vector<int> solveSynConst(const int right, const QueryResult& result) const;
-  int getConstLeft(const QNode* const node) const;
-  int getConstRight(const QNode* const node) const;
-  vector<int> getAllLeft(const QNode* const node, const QueryResult &result) const;
-  vector<int> getAllRight(const QNode* const node, const QueryResult &result) const;
-  bool checkRight(const QNode* const node, const int tested) const;
-  bool checkLeft(const QNode* const node, const int tested) const;
+  bool solveConstConst(const int left, const int right, const QueryResult& result);
+  vector<int> solveConstSyn(const int left, const QueryResult& result);
+  bool solveConstAny(const int left, const QueryResult& result);
 
-  bool findPathToNode(STMTLINE current, STMTLINE end, VARINDEX contextVar, vector<STMTLINE> path) const;
+  vector<int> solveSynConst(const int right, const QueryResult& result);
+  vector<std::pair<int, int>> solveSynSyn(const QueryResult& result);
+  vector<int> solveSynAny(const QueryResult& result);
+
+  bool solveAnyConst(const int right, const QueryResult& result);
+  vector<int> solveAnySyn(const QueryResult& result);
+  bool solveAnyAny(const QueryResult& result);
+
+  bool findPathToNode(STMTLINE current, STMTLINE end, VARINDEX contextVar, vector<STMTLINE> path);
 };
