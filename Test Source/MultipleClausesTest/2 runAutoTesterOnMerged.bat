e7b80b4d1b95261dc614fd18b2655bd3ba2ea067
Jason Sia <swkjason@hotmail.com>
2015-10-14 12:45:59 +0800

    Updated AutoTester With Line Numbers

--- Test Source/MultipleClausesTest/2 runAutoTesterOnMerged.bat
+++ Test Source/MultipleClausesTest/2 runAutoTesterOnMerged.bat
@@ -1,6 +1,6 @@
 echo off
 cls
 echo "Start Test"
-start "" ..\..\EmptyGeneralTesting\Debug\AutoTester.exe Advance_Source.txt QueryMerged.txt QueryMerged.xml
+start "" ..\..\EmptyGeneralTesting\Debug\AutoTester.exe FinalSource.txt QueryMerged.txt QueryMerged.xml
 
 echo "Done: Check Result at QueryMerged.xml"
